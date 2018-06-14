b97efbda695e39d6c58652c64ba9d0b21a35beb9
Neethu <neethuvj74@gmail.com>
2018-06-14 21:01:27 +1000

    express rest api with mongoose part1

---
+++ models/dishes.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+var commentSchema = new mongoose.Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, 
+{ 
+    usePushEach: true 
+},
+{
+    timestamps: true
+});
+
+var dishSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    comments:[commentSchema]
+}, 
+
+{ 
+    usePushEach: true 
+},
+
+{
+    timestamps: true
+    
+}
+
+);
+
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
